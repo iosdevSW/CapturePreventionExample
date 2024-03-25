7f281a43bba9f01694bbc6ce236cc72c656acaea
iosdevSW <iosdev.sw@gmail.com>
2024-03-25 17:18:45 +0900

    Initial Commit

---
+++ CaptureProtectionExample/CaptureProtectionExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureProtectionExampleApp.swift
+//  CaptureProtectionExample
+//
+//  Created by iOS신상우 on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct CaptureProtectionExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaptureProtectionExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CaptureProtectionExample
+//
+//  Created by iOS신상우 on 3/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
